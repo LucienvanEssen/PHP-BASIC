a7db0cb4865321cf28d535180c8a3aa8f5f162c7
Lucien van Essen <2113629@talnet.nl>
2020-06-04 12:23:08 +0200

    door gelezen en kleine oefening

---
+++ 1-Syntax/taak01/syntax.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<head>
+
+<title>syntax oefening</title>
+
+</head>
+
+<body>
+
+<?php
+
+echo " hello world! ";
+$woord = ' hello world ';
+$woord2 = ' hello world? ';
+echo $woord . " ";
+echo $woord . " " . $woord2;
+
+?>
+
+</body>
+
+</html>
