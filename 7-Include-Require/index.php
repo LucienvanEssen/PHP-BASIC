67e4b579f6b698f684a4d5dc39868b2b0aac7d33
Lucien van Essen <lucien_a_2000@hotmail.com>
2020-10-08 15:44:57 +0200

    7-1 liep vast

---
+++ 7-Include-Require/index.php
@@ -0,0 +1,19 @@
+<html>
+<head></head>
+<body>
+
+    <div class="menu">
+        <?php include 'menu.php';?>
+    </div>
+
+</body>
+</html>
+```
+* Include je een PHP file met functies voeg deze dan toe binnen de `<head></head>` tags.
+```html
+<html>
+<head>
+     <title></title>
+     <?php include 'includes/functions.php';?>
+</head>
+</html>
---
+++ 7-Include-Require/taak01/includes/menu.php
@@ -0,0 +1,3 @@
+<?php
+echo "<ol> <li>menu item 1</li> <li>menu item 2</li> <li>menu item 3</li> </ol>";
+?>
